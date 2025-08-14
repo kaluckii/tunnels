500391dd97ba55a57f41ad21896df4620dad10f7
Kaluckii <kaluckii@proton.me>
2025-08-15 02:37:12 +0400

    Initial commit

--- src/modules/init/index.ts
+++ src/modules/init/index.ts
@@ -3,6 +3,6 @@ import { readConfigFile } from '../../config/config.controllers.js';
 export async function runCommand(): Promise<void> {
   // Entry point for initializing tunnels configation file.
 
-  const config = await readConfigFile();
+  await readConfigFile();
   console.log('Configuration file ensured.')
 }
